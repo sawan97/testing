2d24d5bea2a74c0d7e95b6a9e35d169a9f535bca
sawan97 <shrawanverma12345@gmail.com>
2020-06-26 19:28:26 +0530

    delete post and comment (authorized)

--- cotrollers/comments_controller.js
+++ cotrollers/comments_controller.js
@@ -21,3 +21,24 @@ module.exports.create = function(req,res){
         }
     });
 }
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id,function(err,comment){
+        if(comment.user == req.user.id){
+            //get the post id in which post the comments has created
+            let postId = comment.post;
+
+            comment.remove();
+            
+            //find the post and pull out the comment id from the array of comments inside that post
+            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err,post){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    });
+}   
+
+
+
--- routes/comments.js
+++ routes/comments.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 
 const commentsController = require('../cotrollers/comments_controller');
 
-router.post('/create',passport.checkAuthentication, commentsController.create);
+router.post('/create', passport.checkAuthentication, commentsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy);
 
 module.exports = router;
